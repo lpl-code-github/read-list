ccf5c863693a72eb4e42e74e23b51ffd1602c384
李鹏龙 <819508292@qq.com>
2022-12-30 10:35:47 +0800

    Modify the controller returned by the error: add the http status corresponding to the error

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -4,20 +4,23 @@ namespace App\Controller;
 
 use App\Util\Result;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Throwable;
 
 class ErrorController extends AbstractController
 {
     #[Route('/error', name: 'app_error')]
-    public function show(Throwable $exception): JsonResponse
+    public function show(Throwable $exception): Response
     {
         $result = new Result();
+
+        $response = new Response();
+        $response->headers->set('Content-Type',"application/json; charset=UTF-8");
         return match ($exception->getCode()) {
-            400 => $this->json($result->paramError($exception->getMessage())),
-            404 => $this->json($result->notFound($exception->getMessage())),
-            default => $this->json($result->serverError($exception->getMessage())),
+            400 => $response->setContent(json_encode($result->paramError($exception->getMessage())))->setStatusCode(Response::HTTP_BAD_REQUEST),
+            404 => $response->setContent(json_encode($result->notFound($exception->getMessage())))->setStatusCode(Response::HTTP_NOT_FOUND),
+            default => $response->setContent(json_encode($result->serverError($exception->getMessage())))->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR),
         };
     }
 }
