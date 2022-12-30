614f3c561ab91a81a0803da7a1f7a197d36a9bd1
李鹏龙 <819508292@qq.com>
2022-12-30 10:33:26 +0800

    1. Modify DEFAULT_ SIZE 2. Modify BOOK_ SPLIT_ LENGTH 3. Constant to increase book status

--- src/Util/ConstantUtil.php
+++ src/Util/ConstantUtil.php
@@ -4,6 +4,9 @@ const IS_NOT_DELETED = 1;
 const IS_DELETED = 0;
 // 分页默认值
 const DEFAULT_PAGE = 1;
-const DEFAULT_SIZE = 4;
+const DEFAULT_SIZE = 10;
 // 图书每页显示的字数
-const BOOK_SPLIT_LENGTH=300;
+const BOOK_SPLIT_LENGTH=196;
+// 图书状态
+const BOOK_VISIBLE=1;
+const BOOK_INVISIBLE=0;
