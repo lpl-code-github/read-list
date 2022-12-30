1e662225cdaab2a5ee0583f1c0bd6a79958215c0
李鹏龙 <819508292@qq.com>
2022-12-30 10:37:21 +0800

    Add saving method

--- src/Service/BookService.php
+++ src/Service/BookService.php
@@ -2,26 +2,39 @@
 
 namespace App\Service;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
+use App\Util\CustomException\ParamErrorException;
 use App\Vo\PageVo;
-use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class BookService
 {
-    private BookRepository $repository;
+    private BookRepository $bookRepository;
 
     public function __construct(BookRepository $repository)
     {
-        $this->repository = $repository;
+        $this->bookRepository = $repository;
     }
 
     /**
      * 分页查询所有未删除、未被下架的图书
      */
-    public function findBookList(string $searchContent="",int $page=1): PageVo
+    public function findBookList(string $searchContent = "", int $page = 1): PageVo
     {
-        $paginator = $this->repository->findBookList($searchContent, $page);
+        $paginator = $this->bookRepository->findBookList($searchContent, $page);
         $count = $paginator->count();
-        return new PageVo($count,DEFAULT_SIZE,$page,$paginator);
+        return new PageVo($count, DEFAULT_SIZE, $page, $paginator);
+    }
+
+    /**
+     * @throws ParamErrorException
+     */
+    public function saveBook(Book $book): bool
+    {
+        // 查询是否已经存在
+        if ($this->bookRepository->findOneBy(["bookTitle" => $book->getBookTitle(), "bookAuthor" => $book->getBookAuthor()]) !== null) {
+            throw new ParamErrorException("The book already exists");
+        }
+        return $this->bookRepository->save($book,true);
     }
 }
